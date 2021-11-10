106005e7a82a2a43b95ef50ed406e5e2c61c5951
Aleksander <olek_mazur@wp.pl>
2021-11-10 09:28:41 +0100

    Add module poker-common (working)

--- socketPoker/poker-client/src/main/java/poker/socket/java/App.java
+++ socketPoker/poker-client/src/main/java/poker/socket/java/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "hello world!" );
+        System.out.println( "Szablon projektu z metodą main i zależnościami wbudowanymi w wykonywalny jar" );
+        //wywołanie metody generującej hash SHA-512
+        System.out.println("HASH 512 dla słowa test: " + TextUtils.sha512Hash("test"));
     }
 }
