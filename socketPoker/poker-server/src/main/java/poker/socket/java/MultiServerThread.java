b3dcad33d94afa69db90def9b1bae7634a9ff8cc
Aleksander <olek_mazur@wp.pl>
2021-11-11 20:33:09 +0100

    Create multiple clients connection (working)

---
+++ socketPoker/poker-server/src/main/java/poker/socket/java/MultiServerThread.java
@@ -0,0 +1,38 @@
+package poker.socket.java;
+
+import java.net.*;
+import java.io.*;
+
+public class MultiServerThread extends Thread{
+    private Socket socket = null;
+
+    public MultiServerThread(Socket socket) {
+        //super();
+        this.socket = socket;
+    }
+
+    public void run() {
+
+        try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream()));
+        ) {
+            String inputLine, outputLine;
+            Protocol kkp = new Protocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye"))
+                    break;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
