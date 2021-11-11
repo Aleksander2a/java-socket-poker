1d0d3a85eaead28bf7cbcf6c038230d756af1419
Aleksander <olek_mazur@wp.pl>
2021-11-11 12:41:10 +0100

    Add module poker-model (working)

---
+++ socketPoker/poker-model/src/main/java/poker/socket/java/App.java
@@ -0,0 +1,13 @@
+package poker.socket.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
